99fda627bfb24cdb211df77017db6c781df3e32d
AleksandrKosmylev <alex.kosmylev@gmail.com>
2022-07-04 14:03:13 +0300

    fix parsing

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,28 +1,24 @@
 #!/usr/bin/env python
 import argparse
+from gendiff.scripts.get_diffs import get_dict_from_file, get_dicts_difference
+
+
+def generate_diff():
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument("-f", "--format",
+                        help="set format of output")
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    args = parser.parse_args()
+    a = args.first_file
+    b = args.second_file
+    result = get_dicts_difference(get_dict_from_file(a), get_dict_from_file(b))
+    print(result)
 
 
 def main():
-    print('Welcome to the gendiff!')
-
-
-
+    generate_diff()
 
 
 if __name__ == '__main__':
     main()
-
-
-parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-parser.add_argument("-f", "--format",
-help = "set format of output")
-parser.add_argument('first_file', type=str)
-parser.add_argument('second_file', type=str)
-args = parser.parse_args()
-if args.format:
-    print(args.first_file)
-else:
-    print(args.second_file)
-
-
-
