5168f6d9d4cef87c8d9d0fcfa6abc8bb210ac499
AleksandrKosmylev <alex.kosmylev@gmail.com>
2022-09-05 21:23:40 +0300

    update gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import argparse
+import json
 # import yaml
 from gendiff.scripts.get_diffs import get_dict_from_file,\
     get_dicts_difference,\
@@ -11,26 +12,26 @@ def generate_diff(path_1, path_2):
     dict_1 = get_dict_from_file(path_1)
     dict_2 = get_dict_from_file(path_2)
     result = get_dicts_difference(dict_1, dict_2)
-#   return "{\n" + str(yaml.dump(result, sort_keys=False)) + "}"
     return result
 
 
 def print_result():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.')
-
     parser.add_argument("-f", '--format',
                         help=' output format (default: stylish)')
-
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
     args = parser.parse_args()
     a = args.first_file
     b = args.second_file
+    json_object = json.dumps(generate_diff(a, b))
     if args.format == 'stylish':
        stringify(generate_diff(a, b))
     elif args.format == 'plain':
        get_plain_diff(generate_diff(a, b))
+    elif args.format == 'json':
+        json_object
     else:
         stringify(generate_diff(a, b))
 
