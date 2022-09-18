c41a0f0b68166f60a8d76694dc38b9705b740841
AleksandrKosmylev <alex.kosmylev@gmail.com>
2022-09-18 22:37:06 +0300

    restore gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,10 +4,9 @@ import json
 import os
 
 
-#from gendiff.scripts.get_diffs import get_dict_from_file,\
-#    get_dicts_difference, stringify,\
-#    get_plain_diff, convert_to_file, print_file_content
-from gendiff.scripts.get_diffs import *
+from gendiff.scripts.get_diffs import get_dict_from_file,\
+    get_dicts_difference, stringify,\
+    get_plain_diff, convert_to_file, print_file_content
 
 
 def generate_diff(path_1, path_2):
