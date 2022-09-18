e93bdf696c4be18e7c7a73e130bcd5d92ffb6aba
AleksandrKosmylev <alex.kosmylev@gmail.com>
2022-09-18 21:49:22 +0300

    remake

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,48 +1,5 @@
 #!/usr/bin/env python
-import argparse
-import json
-import os
-
-
-#from gendiff.scripts.get_diffs import get_dict_from_file,\
-#    get_dicts_difference, stringify,\
-#    get_plain_diff, convert_to_file, print_file_content
-from gendiff.scripts.get_diffs import *
-
-
-def generate_diff(path_1, path_2):
-    dict_1 = get_dict_from_file(path_1)
-    dict_2 = get_dict_from_file(path_2)
-    result = get_dicts_difference(dict_1, dict_2)
-    return result
-
-
-def print_result():
-    parser = argparse.ArgumentParser(
-        description='Compares two configuration files and shows a difference.')
-    parser.add_argument("-f", '--format',
-                        help='set format of output')
-    parser.add_argument('filepath1', type=str)
-    parser.add_argument('filepath2', type=str)
-    args = parser.parse_args()
-    a = args.filepath1
-    b = args.filepath2
-    data = generate_diff(a, b)
-    if args.format == 'plain':
-        convert_to_file(get_plain_diff, data)
-        print_file_content()
-        os.remove("gendiff/output.json")
-    elif args.format == 'stylish':
-        convert_to_file(stringify, data)
-        print_file_content()
-        os.remove("gendiff/output.json")
-    elif args.format == 'json':
-        jsonStr = json.dumps(data)
-        print(jsonStr)
-    else:
-        convert_to_file(stringify, data)
-        print_file_content()
-        os.remove("gendiff/output.json")
+from gendiff.scripts.ggendiff import *
 
 
 def main():
