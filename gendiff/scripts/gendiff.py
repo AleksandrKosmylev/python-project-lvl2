668441cd43647fb4f9b6d0cd4347b57de8235768
AleksandrKosmylev <alex.kosmylev@gmail.com>
2022-10-01 16:58:12 +0300

    #os.remove(

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -32,18 +32,18 @@ def print_result():
     if args.format == 'plain':
         convert_to_file(get_plain_diff, data)
         print_file_content()
-        os.remove("gendiff/output.json")
+ #       os.remove("gendiff/output.json")
     elif args.format == 'stylish':
         convert_to_file(stringify, data)
         print_file_content()
-        os.remove("gendiff/output.json")
+ #       os.remove("gendiff/output.json")
     elif args.format == 'json':
         jsonStr = json.dumps(data)
         print(jsonStr)
     else:
         convert_to_file(stringify, data)
         print_file_content()
-        os.remove("gendiff/output.json")
+  #      os.remove("gendiff/output.json")
 
 
 def main():
