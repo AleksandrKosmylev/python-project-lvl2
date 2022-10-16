5e8b66a4a7675fe6cb3d9a7c67938d3a98f86c56
AleksandrKosmylev <alex.kosmylev@gmail.com>
2022-10-16 23:41:37 +0300

    return  try

--- gendiff/scripts/get_diffs.py
+++ gendiff/scripts/get_diffs.py
@@ -248,30 +248,30 @@ def get_plain_diff(x):
 def convert_to_file(func, file_difference):
     original_stdout = sys.stdout
     current_directory = os.getcwd()
-    output_file = open(current_directory + "/output.txt", 'w')
+    output_file = open(current_directory + "/output.json", 'w')
     sys.stdout = output_file
     func(file_difference)
     output_file.close()
     sys.stdout = original_stdout
-    with open(current_directory + "/output.txt", 'r') as file:
+    with open(current_directory + "/output.json", 'r') as file:
         filedata = file.read()
     to_replace = {'False': 'false', "True": "true", "None": "null"}
     for i in to_replace.keys():
         filedata = filedata.replace(i, to_replace[i])
-    with open(current_directory + "/output.txt", 'w') as file:
+    with open(current_directory + "/output.json", 'w') as file:
         file.write(filedata)
     if func == stringify:
-        with open(current_directory + "/output.txt", 'r+') as file:
+        with open(current_directory + "/output.json", 'r+') as file:
             lines = file.readlines()
             file.seek(0)
             file.truncate()
             file.writelines(lines[:-1])
-        with open(current_directory + "/output.txt", 'a') as file:
+        with open(current_directory + "/output.json", 'a') as file:
             file.write('}')
 
 
 def print_file_content():
-    with open(current_directory + "/output.txt") as f:
+    with open(current_directory + "/output.json") as f:
             data = f.read()
 #        data = yaml.load(f, Loader=yaml.Loader)
 #        data = json.loads(f)
@@ -302,4 +302,4 @@ def generate_diff(path_1, path_2, formatter='stylish'):
 
 
 current_directory = os.getcwd()
-output_path = current_directory + "/output.txt"
+output_path = current_directory + "/output.json"
