6127d6e81eb53127ee23c2c7dde31dfd7101443e
AleksandrKosmylev <alex.kosmylev@gmail.com>
2022-10-17 00:25:25 +0300

    return  try

--- gendiff/scripts/get_diffs.py
+++ gendiff/scripts/get_diffs.py
@@ -277,7 +277,7 @@ def print_file_content():
             data = f.read()
 #        data = yaml.load(f, Loader=yaml.Loader)
 #        data = json.loads(f)
-    return 888
+    return "888"
 
 
 #    with open(current_directory + "/output.json", 'r') as a_file:
@@ -292,12 +292,12 @@ def generate_diff(path_1, path_2, formatter='stylish'):
     if formatter == 'stylish':
         convert_to_file(stringify, result)
    #     return "7"
- #       print_file_content()
+        print_file_content()
 
     elif formatter == 'plain':
         convert_to_file(get_plain_diff, result)
  #       print_file_content()
- #       return "6"
+        return "6"
 
     elif formatter == 'json':
         jsonStr = json.dumps(result)
