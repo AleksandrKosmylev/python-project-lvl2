1f2f4a61372b0a1b5f0e59bdcf9d848c50dc566a
AleksandrKosmylev <alex.kosmylev@gmail.com>
2022-10-16 20:42:32 +0300

    return  try

--- gendiff/scripts/get_diffs.py
+++ gendiff/scripts/get_diffs.py
@@ -282,11 +282,16 @@ def generate_diff(path_1, path_2, formatter='stylish'):
     result = get_dicts_difference(dict_1, dict_2)
     if formatter == 'stylish':
         convert_to_file(stringify, result)
-        print_file_content()
+        a_file = open(current_directory + "/output.json")
+        a_file.read()
+
+ #       print_file_content()
 
     elif formatter == 'plain':
         convert_to_file(get_plain_diff, result)
-        print_file_content()
+        a_file = open(current_directory + "/output.json")
+        a_file.read()
+ #       print_file_content()
 
     elif formatter == 'json':
         jsonStr = json.dumps(result)
