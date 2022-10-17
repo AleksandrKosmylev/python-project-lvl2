e3db24ea8a95a48a4f6b29f2226893e8e1abf244
AleksandrKosmylev <alex.kosmylev@gmail.com>
2022-10-17 21:55:18 +0300

    return  try

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -34,7 +34,7 @@ def test_get_dict_from_file():
 
 def test_stringify_flat_json():
     generate_diff(path_to_file_1_json, path_to_file_2_json)
-    with open("/gendiff/output.json", 'r') as file_1:
+    with open("tests/fixtures/first_stringify/file1.json", 'r') as file_1:
         with open(path_stringify_flat_json, 'r') as file_2:
             data_1 = file_1.read()
             data_2 = file_2.read()
