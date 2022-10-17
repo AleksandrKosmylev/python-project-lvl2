6c08b13cc56f165703f35b6da60c8895c0f213de
AleksandrKosmylev <alex.kosmylev@gmail.com>
2022-10-17 19:51:32 +0300

    return  try

--- gendiff/scripts/get_diffs.py
+++ gendiff/scripts/get_diffs.py
@@ -273,8 +273,6 @@ def convert_to_file(func, file_difference):
 
 
 def print_file_content():
-    a = '888'
-    return a
 """
     with open(current_directory + "/output.json") as f:
             data = f.read()
@@ -291,7 +289,7 @@ def generate_diff(path_1, path_2, formatter='stylish'):
     result = get_dicts_difference(dict_1, dict_2)
     if formatter == 'stylish':
  #       convert_to_file(stringify, result)
-   #     return "7"
+        return "7"
         print_file_content()
 
     elif formatter == 'plain':
