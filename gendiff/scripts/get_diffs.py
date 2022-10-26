bd9650f094adee234b638ddcb2ebc7fa0b1b83e5
AleksandrKosmylev <alex.kosmylev@gmail.com>
2022-10-26 19:05:02 +0300

    fix length of string

--- gendiff/scripts/get_diffs.py
+++ gendiff/scripts/get_diffs.py
@@ -107,115 +107,115 @@ def get_dicts_diff(dict_1, dict_2):
 
 def stringify(x, spaces='  '):
     def walk(value, acc):
-        tabulation = spaces * acc
+        tab = spaces * acc
         keys_of_tree = ['status', 'value', 'childs', 'old_value']
         if type(value) is dict:
-            for key_of_dict in value.keys():
-                if type(value[key_of_dict]) is dict:
-                    if keys_of_tree != list(value[key_of_dict].keys()):
+            for dict_key in value.keys():
+                if type(value[dict_key]) is dict:
+                    if keys_of_tree != list(value[dict_key].keys()):
                         acc += 1
-                        tabulation = spaces * acc
-                        print(f'{tabulation}{key_of_dict}:', "{")
-                        walk(value[key_of_dict], acc + 1)
+                        tab = spaces * acc
+                        print(f'{tab}{dict_key}:', "{")
+                        walk(value[dict_key], acc + 1)
                         acc -= 1
-                        tabulation = spaces * acc
-                    elif keys_of_tree == list(value[key_of_dict].keys()):
-                        values_list = list(value[key_of_dict].values())
+                        tab = spaces * acc
+                    elif keys_of_tree == list(value[dict_key].keys()):
+                        values_list = list(value[dict_key].values())
                         values_list = values_list
-                        status_value = values_list[0]
+                        status = values_list[0]
                         # branches that depend on status
                         # was added.
                         # check children: if no childs.
-                        # list(value[key_of_dict].values())[2] == 'childs': ""
-                        # (list(value[key_of_dict].values())[1]) == 'value': ""
-                        if status_value == 'was added' and\
+                        # list(value[dict_key].values())[2] == 'childs': ""
+                        # (list(value[dict_key].values())[1]) == 'value': ""
+                        if status == 'was added' and\
                                 values_list[2] == '':
-                            print(f'{tabulation}'
-                                  f'{sigh(status_value)} {key_of_dict}: ',
+                            print(f'{tab}'
+                                  f'{sigh(status)} {dict_key}: ',
                                   end='')
                             print(values_list[1])
                         # check children: if childs exist.
-                        # list(value[key_of_dict].values())[2] ==
+                        # list(value[dict_key].values())[2] ==
                         # 'childs': "{, }"
-                        elif status_value == 'was added' and\
+                        elif status == 'was added' and\
                                 values_list[2] != '':
-                            print(f'{tabulation}'
-                                  f'{sigh(status_value)} {key_of_dict}:', "{")
+                            print(f'{tab}'
+                                  f'{sigh(status)} {dict_key}:', "{")
                             acc += 1
                             walk(values_list[2], acc + 1)
-                            tabulation = spaces * acc
+                            tab = spaces * acc
                             acc -= 1
-                        elif status_value == 'no changes' and\
+                        elif status == 'no changes' and\
                                 values_list[2] == '':
-                            print(f' {tabulation}'
-                                  f'{sigh(status_value)}'
-                                  f'{key_of_dict}: ', end='')
+                            print(f' {tab}'
+                                  f'{sigh(status)}'
+                                  f'{dict_key}: ', end='')
                             print(values_list[1])
-                        elif status_value == 'no changes' and\
+                        elif status == 'no changes' and\
                                 values_list[2] != '':
-                            print(f'{tabulation}'
-                                  f'{sigh(status_value)}'
-                                  f'{key_of_dict}:')
+                            print(f'{tab}'
+                                  f'{sigh(status)}'
+                                  f'{dict_key}:')
                             walk(values_list[2], acc + 1)
-                        elif status_value == 'was updated':
+                        elif status == 'was updated':
                             if values_list[2] == '[**]':
-                                print(f'{tabulation}- {key_of_dict}: ', " {")
+                                print(f'{tab}- {dict_key}: ', " {")
                                 walk(values_list[3], acc + 1)
-                                print(f'{tabulation}+ {key_of_dict}: ')
+                                print(f'{tab}+ {dict_key}: ')
                                 walk(values_list[1], acc + 1)
                             elif values_list[2] == '[_*]':
-                                print(f'{tabulation}- {key_of_dict}: ', end='')
+                                print(f'{tab}- {dict_key}: ', end='')
                                 print(values_list[3])
-                                print(f'{tabulation}+ {key_of_dict}:', "{")
+                                print(f'{tab}+ {dict_key}:', "{")
                                 acc += 1
                                 walk(values_list[1], acc + 1)
                                 acc -= 1
-                                tabulation = spaces * acc
+                                tab = spaces * acc
                             elif values_list[2] == '[*_]':
-                                print(f'{tabulation}- {key_of_dict}:', "{")
+                                print(f'{tab}- {dict_key}:', "{")
                                 acc += 1
                                 walk(values_list[3], acc + 1)
                                 acc -= 1
-                                tabulation = spaces * acc
-                                print(f'{tabulation}+ {key_of_dict}: ', end='')
+                                tab = spaces * acc
+                                print(f'{tab}+ {dict_key}: ', end='')
                                 print(values_list[1])
                             elif values_list[2] == '[__]':
-                                print(f'{tabulation}- {key_of_dict}:', end='')
+                                print(f'{tab}- {dict_key}:', end='')
                                 if values_list[3] == "":
                                     print(" ")
                                 else:
                                     print("", values_list[3])
-                                print(f'{tabulation}+ {key_of_dict}:', end='')
+                                print(f'{tab}+ {dict_key}:', end='')
                                 if values_list[1] == "":
                                     print(" ")
                                 else:
                                     print("", values_list[1])
-                        elif status_value == 'was updated' and\
+                        elif status == 'was updated' and\
                                 values_list[2] != '':
-                            print(f'{tabulation}- {key_of_dict} :')
-                            print(f'{tabulation}+ {key_of_dict} :')
-                        elif status_value == 'was removed' and\
+                            print(f'{tab}- {dict_key} :')
+                            print(f'{tab}+ {dict_key} :')
+                        elif status == 'was removed' and\
                                 values_list[2] == '':
-                            print(f'{tabulation}{sigh(status_value)} {key_of_dict}: ', end='')
+                            print(f'{tab}{sigh(status)} {dict_key}: ', end='')
                             print(values_list[3])
-                        elif status_value == 'was removed' and\
+                        elif status == 'was removed' and\
                                 values_list[2] != '':
-                            print(f'{tabulation}{sigh(status_value)} {key_of_dict}:', "{")
+                            print(f'{tab}{sigh(status)} {dict_key}:', "{")
                             acc += 1
                             walk(values_list[2], acc + 1)
                             acc -= 1
-                            tabulation = spaces * acc
+                            tab = spaces * acc
                     else:
                         print('***')
-                if type(value[key_of_dict]) is not dict:
+                if type(value[dict_key]) is not dict:
                     acc += 1
-                    tabulation = spaces * acc
-                    print(f'{tabulation}{key_of_dict}:', value[key_of_dict])
+                    tab = spaces * acc
+                    print(f'{tab}{dict_key}:', value[dict_key])
                     acc -= 1
-                    tabulation = spaces * acc
+                    tab = spaces * acc
         acc -= 1
-        tabulation = spaces * acc
-        print(tabulation + '}')
+        tab = spaces * acc
+        print(tab + '}')
     print("{")
     return walk(x, 1)
 
@@ -238,16 +238,19 @@ def get_plain_diff(x):
                     # (list(value[key_of_dict].values())[1]) == 'value': ""
                     if status_value == 'was added':
                         # check children: if childs exist.
-                        # list(value[key_of_dict].values())[2] == 'childs': "{, }
+                        # list(value[key_of_dict].values())[2] ==
+                        # 'childs': "{, }
                         if list(value[key_of_dict].values())[2] == '':
                             acc.append(str(key_of_dict))
-                            print("Property", repr(''.join(acc)), status_value, "with value:",
+                            print("Property", repr(''.join(acc)),
+                                  status_value, "with value:",
                                   end=' ')
                             print(repr(list(value[key_of_dict].values())[1]))
                             acc = acc[:-1]
                         elif list(value[key_of_dict].values())[2] != '':
                             acc.append(str(key_of_dict))
-                            print("Property", repr(''.join(acc)), status_value, "with value:", end=' ')
+                            print("Property", repr(''.join(acc)),
+                                  status_value, "with value:", end=' ')
                             print('[complex value]')
                             acc = acc[:-1]
                     elif status_value == 'was updated':
@@ -258,20 +261,25 @@ def get_plain_diff(x):
                             acc = acc[:-1]
                         elif list(value[key_of_dict].values())[2] == '[_*]':
                             acc.append(str(key_of_dict))
-                            print("Property", repr(''.join(acc)), 'was updated. From', end=' ')
-                            print(repr(list(value[key_of_dict].values())[3]), 'to', end=' ')
+                            print("Property", repr(''.join(acc)),
+                                  'was updated. From', end=' ')
+                            print(repr(list(value[key_of_dict].values())[3]),
+                                  'to', end=' ')
                             print('[complex value]')
                             acc = acc[:-1]
                         elif list(value[key_of_dict].values())[2] == '[*_]':
                             acc.append(str(key_of_dict))
-                            print("Property", repr(''.join(acc)), 'was updated. From', end=' ')
+                            print("Property", repr(''.join(acc)),
+                                  'was updated. From', end=' ')
                             print('[complex value] to', end=' ')
                             print(repr(list(value[key_of_dict].values())[1]))
                             acc = acc[:-1]
                         elif list(value[key_of_dict].values())[2] == '[__]':
                             acc.append(str(key_of_dict))
-                            print("Property", repr(''.join(acc)), 'was updated. From', end=' ')
-                            print(repr(list(value[key_of_dict].values())[3]), 'to', end=' ')
+                            print("Property", repr(''.join(acc)),
+                                  'was updated. From', end=' ')
+                            print(repr(list(value[key_of_dict].values())[3]),
+                                  'to', end=' ')
                             print(repr(list(value[key_of_dict].values())[1]))
                             acc = acc[:-1]
                     elif status_value == 'was removed':
