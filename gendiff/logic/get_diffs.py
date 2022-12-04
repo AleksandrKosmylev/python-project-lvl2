c14ff2586384975421803ff8ac105e753206ddce
AleksandrKosmylev <alex.kosmylev@gmail.com>
2022-12-04 18:32:36 +0300

    replace True, False, None

--- gendiff/logic/get_diffs.py
+++ gendiff/logic/get_diffs.py
@@ -2,6 +2,7 @@ from gendiff.logic.get_dicts_diff import get_dicts_diff, get_dict_from_file
 from gendiff.logic.formatters import stringify, get_plain_diff
 import json
 
+
 def generate_diff(path_1, path_2, formatter='stylish'):
     dict_1 = get_dict_from_file(path_1)
     dict_2 = get_dict_from_file(path_2)
