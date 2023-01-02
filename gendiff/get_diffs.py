3fdf82d45ad5403c5197eee3cd6fb51cf641d9b2
AleksandrKosmylev <alex.kosmylev@gmail.com>
2023-01-02 12:04:51 +0300

    major fix structure

--- gendiff/get_diffs.py
+++ gendiff/get_diffs.py
@@ -2,16 +2,16 @@ from gendiff.get_dicts_diff import get_dicts_diff, get_dict_from_file
 from gendiff.formatters import stringify, get_plain_diff
 import json
 
-Stylish = 'stylish'
+# Stylish = 'stylish'
 Plain = 'plain'
 Json = 'json'
 
 
-def generate_diff(path_1, path_2, formatter=Stylish):
+def generate_diff(path_1, path_2, formatter='stylish'):
     dict_1 = get_dict_from_file(path_1)
     dict_2 = get_dict_from_file(path_2)
     result = get_dicts_diff(dict_1, dict_2)
-    if formatter == Stylish:
+    if formatter == 'stylish'
         print(stringify(result))
         return stringify(result)
     elif formatter == Plain:
