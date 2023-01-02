1e4af37621cdd660a0e3019a7ebdd3d874a1ded2
AleksandrKosmylev <alex.kosmylev@gmail.com>
2023-01-02 12:06:59 +0300

    major fix structure

--- gendiff/get_diffs.py
+++ gendiff/get_diffs.py
@@ -11,7 +11,7 @@ def generate_diff(path_1, path_2, formatter='stylish'):
     dict_1 = get_dict_from_file(path_1)
     dict_2 = get_dict_from_file(path_2)
     result = get_dicts_diff(dict_1, dict_2)
-    if formatter == 'stylish'
+    if formatter == 'stylish':
         print(stringify(result))
         return stringify(result)
     elif formatter == Plain:
