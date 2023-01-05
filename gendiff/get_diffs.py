0002c378e004c135bcbddfaaba646fb44e279ad6
AleksandrKosmylev <alex.kosmylev@gmail.com>
2023-01-05 14:23:20 +0300

    delete constants; add exception in case of wrong format

--- gendiff/get_diffs.py
+++ gendiff/get_diffs.py
@@ -2,21 +2,17 @@ from gendiff.get_dicts_diff import get_dicts_diff, get_dict_from_file
 from gendiff.formatters import stringify, get_plain_diff
 import json
 
-Stylish = 'stylish'
-Plain = 'plain'
-Json = 'json'
 
-
-def generate_diff(path_1, path_2, formatter=Stylish):
+def generate_diff(path_1, path_2, formatter='stylish'):
     dict_1 = get_dict_from_file(path_1)
     dict_2 = get_dict_from_file(path_2)
     result = get_dicts_diff(dict_1, dict_2)
-    if formatter == Stylish:
+    if formatter == 'stylish':
         return stringify(result)
-    elif formatter == Plain:
+    elif formatter == 'plain':
         return get_plain_diff(result)
-    elif formatter == Json:
+    elif formatter == 'json':
         output_json = json.dumps(result)
         return output_json
     else:
-        return stringify(result)
+        raise Exception('You\'ve chosen wrong format. Try \'stylish\', \'plain\', \'json\'')
